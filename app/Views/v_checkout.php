c45ed0f7009cf201e161509bc3a937301e7e3689
Angelina <angelinaylf@gmail.com>
2024-07-20 14:40:14 +0700

    menambahkan api data transaksi pembelian

--- app/Views/v_checkout.php
+++ app/Views/v_checkout.php
@@ -116,7 +116,7 @@
                     for (var i = 0; i < results.length; i++) {
                         $("#kabupaten").append($('<option>', {
                             value: results[i]["city_id"],
-                            text: results[i]['city_name'] + " " + results[i]['city_name']
+                            text: results[i]['type'] + " " + results[i]['city_name']
                         }));
                     }
                     hitungTotal();
